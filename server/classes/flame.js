caed2f9af3dfbc6ef62ae8a38741e36bd39a2def
amellagu <amine.mellagui2000@gmail.com>
2025-08-23 12:39:53 +0100

    add flames logic

--- server/classes/flame.js
+++ server/classes/flame.js
@@ -3,18 +3,11 @@ export default class Flame {
         this.x = position.x;
         this.y = position.y;
         this.game = game;
-        this.flameId = Math.random().toString(16);
         this.flamesCounter()
     }
 
     flamesCounter() {
-        console.log("here inside flame with id:", this.flameId)
-        console.log("Board:", this.game.map.board)
-        // should update map when cell contains flames
-        console.log("before", this.game.map.board[this.y][this.x])
         this.game.map.board[this.y][this.x] = 4
-        console.log("after", this.game.map.board[this.y][this.x])
-
 
         setTimeout(() => {
             let key = `${this.x}-${this.y}`;
@@ -23,17 +16,13 @@ export default class Flame {
             if (values.length > 1) {
                 // check if contains more than one flame
                 this.game.flames.get(key).pop()
-                console.log("pooooop")
             } else {
                 // should remove flame from the map 4 => 0
                 this.game.flames.delete(key)
                 this.game.map.board[this.y][this.x] = 0
-                console.log("deleteeeeee")
             }
 
             console.log("game flames inside timeout", this.game.flames.get(key))
-            console.log("last Board:", this.game.map.board)
-
         }, 5000)
     }
 }
